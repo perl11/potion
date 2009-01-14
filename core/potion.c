9b4b91a4786120744492ce88ec722372aa245090
_why <why@whytheluckystiff.net>
2009-01-14 12:00:13 -0800

    * core/potion.c: allocate extra byte for the closing '\0'. was crashing on windows and rightfully so.

--- core/potion.c
+++ core/potion.c
@@ -62,7 +62,7 @@ static void potion_cmd_compile(char *filename, int exec, int verbose) {
     goto done;
   }
 
-  buf = potion_bytes(P, stats.st_size);
+  buf = potion_bytes(P, stats.st_size + 1);
   if (fread(PN_STR_PTR(buf), 1, stats.st_size, fp) == stats.st_size) {
     PN code;
     PN_STR_PTR(buf)[stats.st_size] = '\0';
