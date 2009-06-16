2d40023c2a09693ac7ce44687ef3f657217b79b4
_why <why@whytheluckystiff.net>
2009-06-15 22:48:01 -0700

    * core/lick.c: methods `licks` and `text`.  * test/data/html.pn: test of using lick to build html.

--- core/lick.c
+++ core/lick.c
@@ -16,11 +16,25 @@ PN potion_lick(Potion *P, PN name, PN inner) {
   return (PN)lk;
 }
 
+PN potion_lick_licks(Potion *P, PN cl, PN self) {
+  PN licks = ((struct PNLick *)self)->inner;
+  if (PN_IS_TUPLE(licks)) return licks;
+  return PN_NIL;
+}
+
 PN potion_lick_name(Potion *P, PN cl, PN self) {
   return ((struct PNLick *)self)->name;
 }
 
+PN potion_lick_text(Potion *P, PN cl, PN self) {
+  PN text = ((struct PNLick *)self)->inner;
+  if (PN_IS_STR(text)) return text;
+  return PN_NIL;
+}
+
 void potion_lick_init(Potion *P) {
   PN lk_vt = PN_VTABLE(PN_TLICK);
+  potion_method(lk_vt, "licks", potion_lick_licks, 0);
   potion_method(lk_vt, "name", potion_lick_name, 0);
+  potion_method(lk_vt, "text", potion_lick_text, 0);
 }
