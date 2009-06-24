377a9b0c85ceae7e6d83ae7dc047b761e7595ded
_why <why@whytheluckystiff.net>
2009-06-24 15:36:52 -0700

    * core/primitive.c: casting booleans and nil to numbers.

--- core/primitive.c
+++ core/primitive.c
@@ -13,6 +13,10 @@ static PN potion_nil_is_nil(Potion *P, PN closure, PN self) {
   return PN_TRUE;
 }
 
+static PN potion_bool_number(Potion *P, PN closure, PN self) {
+  return PN_NUM(PN_TEST(self));
+}
+
 static PN potion_bool_string(Potion *P, PN closure, PN self) {
   if (PN_TEST(self)) return potion_str(P, "true");
   return potion_str(P, "false");
@@ -26,6 +30,8 @@ void potion_primitive_init(Potion *P) {
   PN nil_vt = PN_VTABLE(PN_TNIL);
   PN boo_vt = PN_VTABLE(PN_TBOOLEAN);
   potion_method(nil_vt, "nil?", potion_nil_is_nil, 0);
+  potion_method(nil_vt, "number", potion_bool_number, 0);
   potion_send(nil_vt, PN_def, PN_string, potion_str(P, "nil"));
+  potion_method(boo_vt, "number", potion_bool_number, 0);
   potion_method(boo_vt, "string", potion_bool_string, 0);
 }
