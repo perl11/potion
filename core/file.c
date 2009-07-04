aefba266154abd7aca4f575c8e392031b78148ee
_why <why@whytheluckystiff.net>
2009-07-04 15:48:09 -0700

    * core/file.c: load environment variables in the Env table.

--- core/file.c
+++ core/file.c
@@ -9,6 +9,8 @@
 #include "potion.h"
 #include "internal.h"
 
+extern char **environ;
+
 PN potion_file_with(Potion *P, PN cl, PN self, PN path) {
   return PN_NIL;
 }
@@ -23,5 +25,14 @@ PN potion_lobby_read(Potion *P, PN cl, PN self) {
 
 void potion_file_init(Potion *P) {
   // PN file_vt = PN_VTABLE(PN_TFILE);
+  char **env = environ, *key;
+  PN pe = potion_table_empty(P);
+  while (*env != NULL) {
+    for (key = *env; *key != '='; key++);
+    potion_table_put(P, PN_NIL, pe, potion_str2(P, *env, key - *env),
+      potion_str(P, key + 1));
+    env++;
+  }
+  potion_send(P->lobby, PN_def, potion_str(P, "Env"), pe);
   potion_method(P->lobby, "read", potion_lobby_read, 0);
 }
