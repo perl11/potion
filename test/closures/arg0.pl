b75190a3add8859f5907aa4c3242388d4be8a725
Reini Urban <rurban@cpanel.net>
2013-01-29 14:12:52 -0600

    p2: add some preliminary .pl tests (failing)

---
+++ test/closures/arg0.pl
@@ -0,0 +1,7 @@
+sub msg (){ print "no arguments"}
+msg ()
+
+sub msg2 {" and highly sparse"}
+msg2 ()
+
+#=> no arguments and highly sparse
---
+++ test/closures/named.pl
@@ -0,0 +1,7 @@
+sub min (x, y) { y - x }
+
+@b = (99, 98, 97);
+$b[1] = "XXX";
+
+(1, min(y=12, x=89), $b[2], $b[1], "Kirikuro" slice(start=0, end=-2))
+#=> (1, -77, 97, XXX, Kiriku)
