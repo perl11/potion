7341620be2ab793d9037b2870b2713d30118e135
Reini Urban <rurban@cpanel.net>
2013-02-14 11:12:45 -0600

    change (c) from cPanel to perl11

--- core/load.c
+++ core/load.c
@@ -3,7 +3,7 @@
 // loading of external code
 //
 // (c) 2008 why the lucky stiff, the freelance professor
-// (c) 2013 cPanel (rurban)
+// (c) 2013 by perl11 org
 //
 #include <stdio.h>
 #include <stdlib.h>
