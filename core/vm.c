cac51e4dadd6f25cdba9a5b1aa4ba20c669b4e18
Reini Urban <rurban@cpanel.net>
2013-02-28 18:57:25 -0600

    prepare merge with p2 #2

--- core/vm.c
+++ core/vm.c
@@ -242,7 +242,7 @@ reentry:
   if (pos == 0) {
     reg[-1] = reg[0] = self;
     if (f->localsize)
-      memset((void*)locals, 0, sizeof(*locals) * f->localsize);
+      memset((void*)locals, 0, sizeof(PN) * f->localsize);
     if (upc > 0 && upargs != NULL) {
       PN_SIZE i;
       for (i = 0; i < upc; i++) {
