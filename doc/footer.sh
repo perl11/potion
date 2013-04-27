57bf3b9cc4cb1c4bb146ea41b20b5dd7dd120e7f
Reini Urban <rurban@cpanel.net>
2013-04-27 08:27:57 -0500

    doc: less specific footer
    to avoid too many website updates.
    use live doxygen version (use diff. versions on darwin and linux)

--- doc/footer.sh
+++ doc/footer.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-GENERATED=`date`
+GENERATED=`date +"%a %b %d %Y"`
 P2_VERSION=`perl -ane'/P2_VERSION/ && print $F[2]' core/p2.h`
 POTION_DATE=`perl -ane'/POTION_DATE/ && print $F[2]' core/config.h`
+DOXYGEN_VERSION=`doxygen --version`
 
 echo '<hr class="footer"/><address class="footer"><small>'
 echo "Generated on $GENERATED for p2 $P2_VERSION (date=$POTION_DATE)"
-echo 'by <a href="http://www.doxygen.org/index.html">doxygen</a> 1.8.1.2 '
+echo "by <a href='http://www.doxygen.org/index.html'>doxygen</a> $DOXYGEN_VERSION"
 echo '</small></address></body></html>'
