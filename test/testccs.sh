67331baf49d93a66b66bf436688b25f9bba27716
Reini Urban <rurban@cpanel.net>
2013-05-09 10:43:37 -0500

    test/testccs.sh: add platform specific ccs, add -m32 variant

--- test/testccs.sh
+++ test/testccs.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 # test some compiler configurations on this platform (linux, darwin, win)
-CCS="clang clang-mp-3.3 gcc gcc-mp-4.3 gcc-mp-4.8 gcc-mp-4.9"
+# cannot test cross with this yet
+case `uname -o` in
+*Linux) CCS="clang clang2 clang3 gcc gcc-4.4 gcc-4.5 gcc-4.6 gcc-4.7 gcc-4.8" ;;
+Darwin) CCS="clang clang-mp-3.3 gcc gcc-mp-4.3 gcc-mp-4.8 gcc-mp-4.9" ;;
+Cygwin) CCS="clang gcc gcc-3" ;;
+esac
 
 testdebug() {
     make -s realclean
     echo make CC="$1" DEBUG=$2
     make -s CC="$1" DEBUG=$2 >/dev/null 2>/dev/null
     make -s bin/potion bin/potion-test >/dev/null 2>/dev/null
+    make -s bin/p2 bin/p2-test >/dev/null 2>/dev/null
     make test.pn; make test.p2
     echo make CC="$1" DEBUG=$2
     echo ---------------------
@@ -21,3 +27,5 @@ for c in $CCS
 do
     dotest "$c"
 done
+
+dotest "gcc -m32"
