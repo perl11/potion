94be6fa8bc45aea59785f7beaa4996a0cece9119
Reini Urban <rurban@cpanel.net>
2013-05-08 20:19:00 -0500

    sig-default bytecode vm: use potion_sig_at, extend stack when a default is added
    do not clean locals on reentry, clean stack at the beginning.
    calling a functions resets pos to 0, and starts again at reentry.
    we don't want our locals be cleaned away, esp. with nested functions.

--- core/vm.c
+++ core/vm.c
@@ -299,15 +299,15 @@ PN potion_vm(Potion *P, PN proto, PN self, PN vargs, PN_SIZE upc, PN *upargs) {
   // these variables change from proto to proto
   // current = upvals | locals | self | reg
   PN_SIZE pos = 0;
-  long argx = 0;
   PN *args = NULL, *upvals, *locals, *reg;
   PN *current = stack;
 
   if (vargs != PN_NIL) args = PN_GET_TUPLE(vargs)->set;
+  memset((void*)stack, 0, STACK_MAX*sizeof(PN));
+
 reentry:
   if (current - stack >= STACK_MAX) {
-    fprintf(stderr, "all registers used up!");
-    exit(1);
+    potion_fatal("Out of stack, all registers used up!");
   }
 
   upvals = current;
@@ -316,23 +316,19 @@ reentry:
 
   if (pos == 0) {
     reg[-1] = reg[0] = self;
-    if (f->localsize)
-      memset((void*)locals, 0, sizeof(PN) * f->localsize);
     if (upc > 0 && upargs != NULL) {
       PN_SIZE i;
       for (i = 0; i < upc; i++) {
         upvals[i] = upargs[i];
       }
     }
-
     if (args != NULL) {
-      argx = 0;
-      PN_TUPLE_EACH(f->sig, i, v, {
-        if (PN_IS_STR(v)) {
-          PN_SIZE num = PN_GET(f->locals, v);
-          locals[num] = args[argx++];
-        }
-      });
+      long argx;
+      for (argx = 0; argx < potion_sig_arity(P, f->sig); argx++) {
+        PN s = potion_sig_at(P, f->sig, argx);
+        PN_SIZE num = PN_GET(f->locals, PN_TUPLE_AT(s, 0));
+        locals[num] = args[argx];
+      }
     }
   }
   DBG_t("-- run-time --\n");
@@ -499,25 +495,18 @@ reentry:
               reg[op.a] = potion_vm(P, PN_CLOSURE(reg[op.a])->data[0], reg[op.a + 1], argt,
                 PN_CLOSURE(reg[op.a])->extra - 1, &PN_CLOSURE(reg[op.a])->data[1]);
             } else {
+              int i;
               PN sig = PN_CLOSURE(reg[op.a])->sig;
               int numargs = op.b - op.a - 1;
               self = reg[op.a + 1];
               args = &reg[op.a + 2];
-              if ((numargs < PN_CLOSURE(reg[op.a])->arity) && PN_IS_TUPLE(sig)) { // fill in defaults
-		int i = 0, j = -1;
-                vPN(Tuple) t = (vPN(Tuple)) potion_fwd(sig);
-		for (; i < t->len; i++) {
-		  PN v = (PN)t->set[i];
-		  if (PN_IS_STR(v)) j++;
-		  else if (PN_IS_NUM(v) && v == PN_NUM(':')) {
-		    if (j >= numargs  && !reg[op.a + j + 2]) { // and not already filled by NAMED
-		      reg[op.a + j + 2] = (PN)t->set[i+1];
-		      op.b++;
-		    }
-		    if (PN_IS_STR((PN)t->set[i+1])) j--;
-		  }
-		}
-              }
+              if (PN_IS_TUPLE(sig)) {
+                for (i=numargs; i < PN_CLOSURE(reg[op.a])->arity; i++) { // fill in defaults
+                  PN s = potion_sig_at(P, sig, i);
+                  if (s && PN_TUPLE_LEN(s) == 3) { // && !filled by NAMED (?)
+                    reg[op.a + i + 2] = PN_TUPLE_AT(s, 2);
+                    f->stack = PN_NUM(PN_INT(f->stack)+1);
+                    op.b++; }}}
               upc = PN_CLOSURE(reg[op.a])->extra - 1;
               upargs = &PN_CLOSURE(reg[op.a])->data[1];
               current = reg + PN_INT(f->stack) + 2;
