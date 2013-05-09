a78fdcb1e8eb006228a525b44e6e852cd9a1956e
Reini Urban <rurban@cpanel.net>
2013-05-09 10:05:31 -0500

    gc-test: allow also 2 allocations (needed on certain platforms)

--- test/api/gc-test.c
+++ test/api/gc-test.c
@@ -29,7 +29,7 @@ void gc_test_alloc1(CuTest *T) {
   PN ptr = (PN)potion_gc_alloc(P, PN_TUSER, 16);
   PN_SIZE count = potion_mark_stack(P, 0);
   CuAssert(T, "couldn't allocate 16 bytes from GC", PN_IS_PTR(ptr));
-  CuAssertIntEquals(T, "only one allocation should be found", 1, count);
+  CuAssert(T, "only one or two allocations should be found", count >= 1 && count <= 2);
 }
 
 void gc_test_alloc4(CuTest *T) {
