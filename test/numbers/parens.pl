86befe151e11eac1eb67af2a48a084745f58d846
Reini Urban <rurban@cpanel.net>
2013-08-13 10:22:10 +0300

    test/numbers/parens.pl comment

--- test/numbers/parens.pl
+++ test/numbers/parens.pl
@@ -1,9 +1,9 @@
 # ./p2 -Dv test/numbers/parens.pl
 # -- parsed --
-# code  (expr (times (list (expr (plus (value (9 undef undef) value (1 undef undef))))
-#                    list (expr (minus (value (6 undef undef) value (4 undef undef)))))))
+# code  (expr (times (list (expr (plus (value (9) value (1))))
+#                     list (expr (minus (value (6) value (4)))))))
 
-# TODO: (times (expr (list (plus (expr (value (9 nil nil)) expr (value (1 nil nil))))) 
-#              expr (list (minus (expr (value (6 nil nil)) expr (value (4 nil nil)))))))
+# TODO: (times (expr (list (plus (expr (value (9)) expr (value (1)))))
+#               expr (list (minus (expr (value (6)) expr (value (4)))))))
 
 (9 + 1) * (6 - 4)  #=> 20
