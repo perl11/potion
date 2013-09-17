perl11/potion

commit c24f9a15f88611e335077843aea59e6174f56f99

parse: store fileno and lineno in AST
also fix call expr, take call[0] from TUP

core/ast.c
@@ -30,11 +30,13 @@ const int potion_ast_sizes[] = {
 ///\param p AST type
 ///\param a,b,c AST operands
 ///\returns a new three-address op AST leaf
-PN potion_source(Potion *P, u8 p, PN a, PN b, PN c) {
+PN potion_source(Potion *P, u8 p, PN a, PN b, PN c, int lineno) {
   int size = potion_ast_sizes[p];
   // TODO: potion_ast_sizes[p] * sizeof(PN) (then fix gc_copy)
   vPN(Source) t = PN_ALLOC_N(PN_TSOURCE, struct PNSource, 0 * sizeof(PN));
   t->part = (enum PN_AST)p;
+  t->loc.fileno = P->fileno; // only advanced by load/require
+  t->loc.lineno = lineno;
 #if 1
   switch (size) {
   case 3: t->a[2] = PN_SRC(c);

core/ast.h
@@ -18,10 +18,10 @@ typedef struct {
 
 #define PN_TOK_MISSING 0x10000
 
-#define PN_AST(T, A)        potion_source(P, AST_##T, A, PN_NIL, PN_NIL)
-#define PN_AST2(T, A, B)    potion_source(P, AST_##T, A, B, PN_NIL)
-#define PN_AST3(T, A, B, C) potion_source(P, AST_##T, A, B, C)
-#define PN_OP(T, A, B)      potion_source(P, T, A, B, PN_NIL)
+#define PN_AST(T, A)        potion_source(P, AST_##T, A, PN_NIL, PN_NIL, 0)
+#define PN_AST2(T, A, B)    potion_source(P, AST_##T, A, B, PN_NIL, 0)
+#define PN_AST3(T, A, B, C) potion_source(P, AST_##T, A, B, C, 0)
+#define PN_OP(T, A, B)      potion_source(P, T, A, B, PN_NIL, 0)
 #define PN_TUPIF(T)   PN_IS_TUPLE(T) ? T : PN_TUP(T)
 #define PN_SRC(S)     ((struct PNSource *)S)
 #define PN_PART(S)    ((struct PNSource *)S)->part
@@ -38,6 +38,6 @@ typedef struct {
     } \
   })
 
-PN potion_source(Potion *, u8, PN, PN, PN);
+PN potion_source(Potion *, u8, PN, PN, PN, int);
 
 #endif

core/internal.c
@@ -18,6 +18,7 @@ PN PN_cmp, PN_number, PN_name, PN_length, PN_size, PN_STR0;
 #ifdef P2
 PN PN_use, PN_no;
 #endif
+PN pn_filenames;
 
 PN potion_allocate(Potion *P, PN cl, PN self, PN len) {
   struct PNData *obj = PN_ALLOC_N(PN_TUSER, struct PNData, PN_INT(len));
@@ -90,7 +91,6 @@ static void potion_init(Potion *P) {
 
   potion_def_method(P, 0, vtable, PN_lookup, PN_FUNC(potion_lookup, 0));
   potion_def_method(P, 0, vtable, PN_def, PN_FUNC(potion_def_method, "name=S,block=&"));
-
   potion_send(vtable, PN_def, PN_allocate, PN_FUNC(potion_allocate, 0));
   potion_send(vtable, PN_def, PN_delegated, PN_FUNC(potion_delegated, 0));
 
@@ -111,6 +111,8 @@ static void potion_init(Potion *P) {
   potion_file_init(P);
   potion_loader_init(P);
 
+  pn_filenames = PN_TUP0();
+
   GC_PROTECT(P);
 }
 
@@ -121,6 +123,7 @@ Potion *potion_create(void *sp) {
   PN_FLEX_NEW(P->vts, PN_TFLEX, PNFlex, TYPE_BATCH_SIZE);
   PN_FLEX_SIZE(P->vts) = PN_TYPE_ID(PN_TUSER) + 1;
   P->prec = PN_PREC;
+  P->fileno = -1;
   P->flags = (Potion_Flags)EXEC_VM;
   potion_init(P);
   return P;

core/potion.h
@@ -421,7 +421,16 @@ enum PN_AST {
 };
 struct PNSource {
   PN_OBJECT_HEADER;  	///< PNType vt; PNUniq uniq
-  enum PN_AST part;     ///< AST type, avoid -Wswitch
+  struct {
+#if __WORDSIZE != 64
+    PNType fileno:16;
+    PNType lineno:16;
+#else
+    PNType fileno:32;
+    PNType lineno:32;
+#endif
+  } loc;                ///< bitfield of fileno and lineno
+  enum PN_AST part;     ///< AST type, avoid -Wswitch (aligned access: 4+4+8+4+24)
   struct PNSource * volatile a[3];///< PNTuple of 1-3 kids, \see ast.c
 };
 
@@ -620,6 +629,7 @@ struct Potion_State {
   PN input, source, decl;  ///< parser input, output (AST) and 1st pass declarations
   int yypos;               ///< parser buffer position
   PNAsm * volatile pbuf;   ///< parser buffer
+  PN_SIZE fileno;          ///< currently parsed file
   PN unclosed;             ///< used by parser for named block endings
   PN call, callset;        ///< generic call and callset
   int prec;                ///< decimal precision
@@ -747,6 +757,7 @@ extern PN PN_allocate, PN_break, PN_call, PN_class, PN_compile,
   PN_while;
 extern PN PN_add, PN_sub, PN_mult, PN_div, PN_rem, PN_bitn, PN_bitl, PN_bitr;
 extern PN PN_cmp, PN_number, PN_name, PN_length, PN_size, PN_STR0;
+extern PN pn_filenames;
 
 /// zero values per type
 static inline PN potion_type_default(char type) {
@@ -854,6 +865,8 @@ PN potion_source_dump(Potion *, PN, PN, PN, PN);
 PN potion_source_dumpbc(Potion *, PN, PN, PN);
 PN potion_greg_parse(Potion *, PN);
 PN potion_sig_string(Potion *, PN, PN);
+PN potion_filename_find(Potion *, PN);
+PN potion_filename_push(Potion *, PN);
 
 Potion *potion_gc_boot(void *);
 void potion_lobby_init(Potion *);

syn/syntax-p5.y
@@ -16,6 +16,15 @@
 #include "asm.h"
 #include "ast.h"
 
+#undef PN_AST
+#undef PN_AST2
+#undef PN_AST3
+#undef PN_OP
+#define PN_AST(T, A)        potion_source(P, AST_##T, A, PN_NIL, PN_NIL, G->lineno)
+#define PN_AST2(T, A, B)    potion_source(P, AST_##T, A, B, PN_NIL, G->lineno)
+#define PN_AST3(T, A, B, C) potion_source(P, AST_##T, A, B, C, G->lineno)
+#define PN_OP(T, A, B)      potion_source(P, T, A, B, PN_NIL, G->lineno)
+
 #define YYSTYPE PN
 #define YY_XTYPE Potion *
 #define YY_XVAR P
@@ -214,7 +223,7 @@ expr = c:method  	        { $$ = PN_AST(EXPR, c) }
     | m:special l:list b:block  { PN_SRC(m)->a[1] = PN_SRC(l); PN_SRC(m)->a[2] = PN_SRC(b);
             $$ = PN_AST(EXPR, PN_TUP(m)) }
     | c:calllist		{ $$ = PN_AST(EXPR, c) }
-    | c:call e:expr 		{ $$ = PN_AST(EXPR, PN_PUSH(PN_S(e,0), PN_S(c,0))); }
+    | c:call e:expr 		{ $$ = PN_AST(EXPR, PN_PUSH(PN_S(e,0), PN_TUPLE_AT(c,0))); }
     | c:call l:listexprs 	{ $$ = PN_SHIFT(PN_S(l,0));
             if (!PN_S(l, 0)) { PN_SRC(c)->a[1] = PN_SRC($$); }
             $$ = PN_PUSH(PN_TUP($$), c); }
@@ -437,8 +446,7 @@ comment	= '#' (!end-of-line utf8)*
 # \240 U+A0 NO-BREAK SPACE
 # \205 U+85 NEL
 space = ' ' | '\f' | '\v' | '\t' | '\205' | '\240' | end-of-line
-end-of-line = '\r\n' | '\n' | '\r'
-    { ++G->lineno; $$ = PN_AST2(DEBUG, PN_NUM(G->lineno), PN_NIL) }
+end-of-line = '\r\n' | '\n' | '\r'   { ++G->lineno }
 end-of-file = !'\0'
 # FIXME: starting wordchar (no numbers) + wordchars
 id = < IDFIRST utfw* > { $$ = PN_STRN(yytext, yyleng) }
@@ -465,11 +473,11 @@ utfw = [A-Za-z0-9_]
 #     | [\340-\357] [\200-\277] [\200-\277]
 #     | [\360-\364] [\200-\277] [\200-\277] [\200-\277]
 
-# TODO had \n\r in it, skipping lineno counter
 utf8 = [\t\40-\176]
      | [\302-\337] [\200-\277]
      | [\340-\357] [\200-\277] [\200-\277]
      | [\360-\364] [\200-\277] [\200-\277] [\200-\277]
+     | end-of-line
 
 # for potion_sig, used in the runtime initialization
 sig = args+ end-of-file
@@ -529,6 +537,7 @@ PN p2_parse(Potion *P, PN code, char *filename) {
   yydebug = P->flags;
 
   G->filename = filename;
+  P->fileno = PN_PUT(pn_filenames, PN_STR(filename));
   if (!YY_NAME(parse)(G)) {
     YY_ERROR(G, "** Syntax error");
     fprintf(stderr, "%s", PN_STR_PTR(code));

syn/syntax.y
@@ -12,6 +12,15 @@
 #include "asm.h"
 #include "ast.h"
 
+#undef PN_AST
+#undef PN_AST2
+#undef PN_AST3
+#undef PN_OP
+#define PN_AST(T, A)        potion_source(P, AST_##T, A, PN_NIL, PN_NIL, G->lineno)
+#define PN_AST2(T, A, B)    potion_source(P, AST_##T, A, B, PN_NIL, G->lineno)
+#define PN_AST3(T, A, B, C) potion_source(P, AST_##T, A, B, C, G->lineno)
+#define PN_OP(T, A, B)      potion_source(P, T, A, B, PN_NIL, G->lineno)
+
 #define YYSTYPE PN
 #define YY_XTYPE Potion *
 #define YY_XVAR P
@@ -187,10 +196,11 @@ utfw = [A-Za-z0-9_$@;`{}]
      | [\305-\337] [\200-\277]
      | [\340-\357] [\200-\277] [\200-\277]
      | [\360-\364] [\200-\277] [\200-\277] [\200-\277]
-utf8 = [\t\r\n\40-\176]
+utf8 = [\t\40-\176]
      | [\302-\337] [\200-\277]
      | [\340-\357] [\200-\277] [\200-\277]
      | [\360-\364] [\200-\277] [\200-\277] [\200-\277]
+     | end-of-line
 
 comma = ','
 block-start = ':' --
@@ -320,7 +330,7 @@ sep = (end-of-line | comma) (space | comment | end-of-line | comma)*
 comment	= '#' (!end-of-line utf8)*
 space = ' ' | '\f' | '\v' | '\t'
 end-of-line = ( '\r\n' | '\n' | '\r' )
-    { if (yydebug & EXEC_DEBUG)
+    { ++G->lineno; if (yydebug & EXEC_DEBUG)
         $$ = PN_AST3(DEBUG, $$, PN_NUM(G->lineno), potion_str(P, G->filename)); }
 end-of-file = !.
 
@@ -363,6 +373,7 @@ PN potion_parse(Potion *P, PN code, char *filename) {
 
   yydebug = P->flags;
   G->filename = filename;
+  P->fileno = PN_PUT(pn_filenames, PN_STR(filename));
   if (!YY_NAME(parse)(G)) {
     YY_ERROR(G, "** Syntax error");
     fprintf(stderr, "%s", PN_STR_PTR(code));

commit d782ab9b01bb7bd6b17c28ba5d6e8683d673261e

fix call expr change in optional sigs, fix source_file
c24f9a15f886 broke sig_compile for optional args
print \n inside source_string if the lineno changes within
add (unused) potion_source_loc => buf

core/ast.c
@@ -75,7 +75,7 @@ PN potion_source_name(Potion *P, PN cl, PN self) {
 ///\returns filename of associated AST
 PN potion_source_file(Potion *P, PN cl, PN self) {
   vPN(Source) t = (struct PNSource *)potion_fwd(self);
-  return PN_GET(pn_filenames, t->loc.fileno);
+  return PN_TUPLE_AT(pn_filenames, t->loc.fileno);
 }
 
 ///\memberof PNSource
@@ -93,12 +93,13 @@ PN potion_source_string(Potion *P, PN cl, PN self) {
   int i, n, cut = 0;
   vPN(Source) t = (struct PNSource *)potion_fwd(self);
   PN out = potion_byte_str(P, potion_ast_names[t->part]);
+  int lineno = t->loc.lineno;
   n = potion_ast_sizes[t->part];
   for (i = 0; i < n; i++) {
     pn_printf(P, out, " ");
     if (i == 0 && n > 1) pn_printf(P, out, "(");
     else if (i > 0) {
-      if (t->a[i] == PN_NIL) { // omit subsequent nils
+      if (!t->a[i]) { // omit subsequent nils
 	if (!cut) cut = PN_STR_LEN(out);
       }
       else cut = 0;
@@ -107,8 +108,13 @@ PN potion_source_string(Potion *P, PN cl, PN self) {
       pn_printf(P, out, "\"");
       potion_bytes_obj_string(P, out, (PN)t->a[i]);
       pn_printf(P, out, "\"");
-    } else
+    } else {
       potion_bytes_obj_string(P, out, (PN)t->a[i]);
+    }
+    if ((PN_TYPE(t->a[i]) == PN_TSOURCE) && (t->a[i]->loc.lineno > lineno)) {
+      pn_printf(P, out, "\n");
+      lineno = t->a[i]->loc.lineno;
+    }
     if (i == n - 1 && n > 1) {
       if (cut > 0) {
 	vPN(Bytes) b = (struct PNBytes *)potion_fwd(out);
@@ -124,6 +130,14 @@ PN potion_source_string(Potion *P, PN cl, PN self) {
       pn_printf(P, out, ")");
     }
   }
+  return PN_STR_B(out);
+}
+
+PN potion_source_loc(Potion *P, PN cl, PN self) {
+  PN out = potion_byte_str(P, "");
+  pn_printf(P, out, "%s:%ld",
+            PN_STR_PTR(potion_source_file(P, cl, self)),
+            PN_INT(potion_source_line(P, cl, self)));
   return PN_STR_B(out);
 }
 

core/compile.c
@@ -971,9 +971,9 @@ PN potion_sig_compile(Potion *P, vPN(Proto) f, PN src) {
 	  }
         } else if (rhs->part == AST_ASSIGN && rhs->a[0]->part == AST_PIPE) {
           //x=N|y=o => (assign (pipe (expm x) (expm y)) (expm o))
-          lhs = SRC_TUPLE_AT(rhs, 0);
+          lhs = PN_S_(PN_S_(rhs, 0), 0);
 	  if (lhs->part == AST_EXPR && SRC_TUPLE_AT(lhs,0)->part == AST_MSG) {
-	    PN v = PN_NUM(PN_STR_PTR(PN_S(PN_S(lhs,0),0)->a[0])[0]); // = type. TODO: one-char => VTABLE
+	    PN v = PN_NUM(PN_STR_PTR((PN)SRC_TUPLE_AT(lhs,0)->a[0])[0]); // = type. TODO: one-char => VTABLE
 	    DBG_c("%s)\n", AS_STR(v));
 	    sig = PN_PUSH(sig, v);
 	  }