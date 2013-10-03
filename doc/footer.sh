0cd3e916db79307c7dd31aa9f6d41a1548e8ecdd
Reini Urban <rurban@cpanel.net>
2013-10-03 14:54:16 -0500

    layout fixes: doc/footer.sh

--- doc/footer.sh
+++ doc/footer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo '<hr class="footer"/><address class="footer"><small>'
-echo Generated on `date` for `./potion --version|cut -f1-3 -d' '`
+echo Generated on `date` for `bin/potion --version|cut -f1-3 -d' '`
 echo ') by <a href="http://www.doxygen.org/index.html">doxygen</a> ' `doxygen --version`
 echo '</small></address></body></html>'
