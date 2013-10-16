33266d491c5975d3ca301cd6b00da31944948b15
Reini Urban <rurban@cpanel.net>
2013-10-16 14:21:25 -0500

    update nbody.pn and mk-release from potion

--- tools/mk-release.sh
+++ tools/mk-release.sh
@@ -1,21 +1,34 @@
 #!/bin/sh
-case `uname -s` in
-*Linux) # native to x86_64, cross to i686 via -m32, i686-w64-mingw32-gcc and x86_64-w64-mingw32-gcc
-        CC="gcc-4.8" #clang-3.3"
-        CROSS="i686-w64-mingw32-gcc x86_64-w64-mingw32-gcc" ;;
-Darwin) # native clang not stable enough (16byte %esp alignment), use ports gcc
-        CC="gcc-mp-4.8"
-        CROSS="i386-mingw32-gcc" ;;
-CYGWIN*) # native via gcc4
-        CC="gcc-4"
-        if [ `uname -m` = x86_64 ]; then #Cygwin64
-            CC="gcc"
-        fi
-        ;;
-esac
-#LATER evtl.: ppc, arm, darwin pkg
+# OPTIONS: -ci686-w64-mingw32-gcc ...
+if [ -z "$1" ]; then
+    case `uname -s` in
+        *Linux) # native to x86_64, cross to i686 via -m32 and win32 via i686-w64-mingw32-gcc
+            CC="gcc-4.8" #clang-3.3"
+            CROSS="i686-w64-mingw32-gcc" ;; # x86_64-w64-mingw32-gcc win64 not yet, w64 not used for dist
+        Darwin)
+            CC="clang-mp-3.3" #gcc-mp-4.8"
+            CROSS="i386-mingw32-gcc" ;;
+        CYGWIN*) # native via gcc4
+            CC="gcc-4"
+            if [ `uname -m` = x86_64 ]; then #Cygwin64
+                CC="gcc"
+            fi
+            ;;
+    esac
+    #LATER evtl.: ppc, arm, darwin pkg, win64 port
+else
+    while getopts "c:" opt
+    do
+        if [ "$opt" = "c" ]; then CROSS="$CROSS ${OPTARG}"; fi
+        shift
+    done
+    OPTS=1
+    CC=$@
+fi
 
 dorelease() {
+    echo
+    echo RELEASE $1
     make realclean
     echo make CC="$1"
     make CC="$1" DEBUG=0
@@ -24,14 +37,14 @@ dorelease() {
 }
 
 docross() {
+    echo CROSS $1
     make clean
-    rm config.inc
-    echo make config CC="$1"
-    make -s -f config.mak CC="$1" DEBUG=0
-    make -s core/config.h
+    make clean -C 3rd/libuv
+    rm config.inc lib/libpotion* 3rd/libuv/Makefile
+    echo make CC="$1" DEBUG=0 CROSS=1
+    make -s -f config.mak CC="$1" DEBUG=0 CROSS=1
     touch syn/greg syn/syntax.c syn/syntax-p5.c
-    echo make CC="$1"
-    make CC="$1" DEBUG=0
+    make CC="$1" DEBUG=0 CROSS=1
     make dist
 }
 
@@ -39,15 +52,19 @@ for c in $CC; do
     dorelease "$c"
 done
 
-# build greg and syntax.c native
-make clean
-make syn/syntax.c
-make syn/syntax-p5.c
+if [ -n "$CROSS" ]; then
+    # build greg and syntax.c native
+    make clean
+    make core/syntax.c
 
-for c in $CROSS; do
-    docross "$c"
-done
+    for c in $CROSS; do
+        docross "$c"
+    done
+fi
 
-case `uname -s` in
-  *Linux) dorelease "gcc -m32" ;;
-esac
+if [ -z "$OPTS" ]; then
+    case `uname -s` in
+        *Linux) rm 3rd/libuv/Makefile
+                dorelease "gcc -m32" ;;
+    esac
+fi
