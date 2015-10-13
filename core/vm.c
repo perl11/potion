f2726e95fcc92eeaa6eeb937866f82f1c2474831
Peter Arthur <pete.arthur@gmail.com>
2015-10-13 12:59:59 +0100

    Cache the arity of the function

--- core/vm.c
+++ core/vm.c
@@ -645,7 +645,8 @@ reentry:
     }
     if (args != NULL) {
       long argx;
-      for (argx = 0; argx < potion_sig_arity(P, f->sig); argx++) {
+      int arity = potion_sig_arity(P, f->sig);
+      for (argx = 0; argx < arity; argx++) {
         PN s = potion_sig_name_at(P, f->sig, argx);
         PN_SIZE num = PN_GET(f->locals, s);
         if (num != PN_NONE)
