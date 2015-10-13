d698e8812d0300006714aa7bfba23277a970344d
Peter Arthur <pete.arthur@gmail.com>
2015-10-13 13:06:22 +0100

    Add "methods" method to Lobby

--- core/objmodel.c
+++ core/objmodel.c
@@ -427,6 +427,20 @@ PN potion_message(Potion *P, PN rcv, PN msg) {
   return cl;
 }
 
+PN potion_lobby_methods(Potion *P, PN closure, PN self) {
+  PNType t = PN_TYPE(self);
+  if (!PN_TYPECHECK(t)) return PN_NIL;
+  if (t == PN_TVTABLE && !PN_IS_METACLASS(self)) {
+    //self = (PN)((struct PNVtable *)self)->meta;
+  } else {
+    self = PN_VTABLE(t);
+  }
+  vPN(Vtable) vt = (struct PNVtable *)self;
+  if(vt->methods != NULL)
+    return (PN)vt->methods;
+  return PN_NIL;
+}
+
 PN potion_obj_add(Potion *P, PN a, PN b) {
   return potion_send(a, PN_add, b);
 }
@@ -722,4 +736,5 @@ void potion_lobby_init(Potion *P) {
   potion_method(P->lobby, "can",   potion_lobby_can, "method=S");
   potion_method(P->lobby, "print", potion_lobby_print, 0);
   potion_method(P->lobby, "say",   potion_lobby_say, 0);
+  potion_method(P->lobby, "methods", potion_lobby_methods, 0);
 }
